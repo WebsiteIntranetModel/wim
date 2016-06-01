bb79533e367c191cea5501c8f8a1c32c52edc068
Bram ten Hove <bram.ten.hove@goalgorilla.com>
2016-06-01 11:34:09 +0200

    Created simple install profile + installation script

---
+++ dev-scripts/install/install-script.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Install script for in the docker container.
+cd /var/www/html/;
+
+if [ ${1:-"install"} = "reinstall" ]; then
+  # We received an argument to re-install the site.
+  drush sql-drop -y;
+  rm -f sites/default/settings.php;
+  rm -rf sites/default/files;
+  echo "Database dropped, files folder and settings.php removed"
+fi
+
+# Install the site using the WIM installation profile.
+drush -y site-install wim --db-url=mysql://root:root@db:3306/wim --account-pass=admin install_configure_form.site_name='WIM';
+echo "Drupal installation complete"
+
+# Set ownership for data folder.
+chown -R www-data:www-data /var/www/html/
+echo "Correct ownership of the docroot has been set"
+
+# Set correct permission for the settings.php file.
+chmod 444 sites/default/settings.php
+echo "Restored read-only permissions for settings.php"
+
+# Clear drush cache.
+drush cc drush
