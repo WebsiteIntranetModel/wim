02388c20a5a593df0452458d2c214572abb96d6c
Bram ten Hove <bram.ten.hove@goalgorilla.com>
2016-06-02 21:44:49 +0200

    Run Code Sniffer also on themes folder

--- dev-scripts/check-coding-standards.sh
+++ dev-scripts/check-coding-standards.sh
@@ -2,4 +2,8 @@
 
 # Run PHP Code Sniffer on WIM profile.
 echo "Running PHP Code Sniffer on /profiles/wim."
-phpcs --standard=Drupal --extensions=php,module,inc,install,test,profile,theme /var/www/html/profiles/wim
+phpcs --report=summary --standard=Drupal --extensions=php,module,inc,install,test,profile,theme /var/www/html/profiles/wim
+
+# Run PHP Code Sniffer on custom themes folder.
+echo "Running PHP Code Sniffer on /sites/all/themes."
+phpcs --report=summary --standard=Drupal --extensions=php,module,inc,install,test,profile,theme /var/www/html/sites/all/themes
