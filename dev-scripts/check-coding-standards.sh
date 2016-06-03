946fa16acbb92c915fa6c27d98255f5d72d96d44
Bram ten Hove <bram.ten.hove@goalgorilla.com>
2016-06-03 13:30:24 +0200

    Fixed issue where exit 1 code would be overwritten by exit 0 code later on for coding standards

--- dev-scripts/check-coding-standards.sh
+++ dev-scripts/check-coding-standards.sh
@@ -6,10 +6,6 @@ if [ ${1:-"summary"} = "full" ]; then
   REPORT=--report-full
 fi
 
-# Run PHP Code Sniffer on WIM profile.
-echo "Running PHP Code Sniffer on /profiles/wim."
-phpcs $REPORT --standard=Drupal --extensions=php,module,inc,install,test,profile,theme /var/www/html/profiles/wim
-
-# Run PHP Code Sniffer on custom themes folder.
-echo "Running PHP Code Sniffer on /sites/all/themes."
-phpcs $REPORT --standard=Drupal --extensions=php,module,inc,install,test,profile,theme --ignore=*/contrib/bootstrap/* /var/www/html/sites/all/themes
+# Run PHP Code Sniffer.
+echo "Running PHP Code Sniffer."
+phpcs $REPORT --standard=Drupal --extensions=php,module,inc,install,test,profile,theme --ignore=*/contrib/bootstrap/* /var/www/html/profiles/wim /var/www/html/sites/all/themes
