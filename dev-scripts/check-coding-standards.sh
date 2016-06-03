4fbc34e35c2f53e9b0daa65452cfb6a3482db516
Bram ten Hove <bram.ten.hove@goalgorilla.com>
2016-06-03 12:04:56 +0200

    Added example unit test to be executed

--- dev-scripts/check-coding-standards.sh
+++ dev-scripts/check-coding-standards.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+REPORT=--report-summary
+
+if [ ${1:-"summary"} = "full" ]; then
+  REPORT=--report-full
+fi
+
 # Run PHP Code Sniffer on WIM profile.
 echo "Running PHP Code Sniffer on /profiles/wim."
-phpcs --report=summary --standard=Drupal --extensions=php,module,inc,install,test,profile,theme /var/www/html/profiles/wim
+phpcs $REPORT --standard=Drupal --extensions=php,module,inc,install,test,profile,theme /var/www/html/profiles/wim
 
 # Run PHP Code Sniffer on custom themes folder.
 echo "Running PHP Code Sniffer on /sites/all/themes."
-phpcs --report=summary --standard=Drupal --extensions=php,module,inc,install,test,profile,theme --ignore=*/contrib/bootstrap/* /var/www/html/sites/all/themes
+phpcs $REPORT --standard=Drupal --extensions=php,module,inc,install,test,profile,theme --ignore=*/contrib/bootstrap/* /var/www/html/sites/all/themes
---
+++ dev-scripts/tests/unit-tests.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Run simple tests for WIM.
+php /var/www/html/scripts/run-tests.sh WIM
