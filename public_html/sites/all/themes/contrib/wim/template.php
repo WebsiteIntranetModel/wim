1a325f299e9ef0a0cfadfa5ecac33a0e951e5177
Peter Polman <info@peterpolman.nl>
2016-06-07 16:54:34 +0200

    Removed php code

--- public_html/sites/all/themes/contrib/wim/template.php
+++ public_html/sites/all/themes/contrib/wim/template.php
@@ -1,8 +1,1 @@
 <?php
-/**
- * Declare various hook_*_alter() hooks.
- *
- * hook_*_alter() implementations must live (via include) inside this file so
- * they are properly detected when drupal_alter() is invoked.
- */
-bootstrap_include('wim', 'includes/alter.inc');
