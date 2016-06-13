245e6200feac625fe2de640aaf2b74825e7aa6b4
Roman Paska <roman.paska@lemberg.co.uk>
2016-06-13 15:01:04 +0300

    Readd WIM theme to the profile

---
+++ themes/wim/gulpfile.js
@@ -0,0 +1,249 @@
+// ===================================================
+// Required packages
+// ===================================================
+
+var gulp          = require('gulp'),
+    postcss       = require('gulp-postcss'),
+    sass          = require('gulp-sass'),
+    sourcemaps    = require('gulp-sourcemaps'),
+    gulpignore    = require('gulp-ignore'),
+    autoprefixer  = require('autoprefixer'),
+    mqpacker      = require('css-mqpacker'),
+    precss        = require('precss'),
+    lost          = require('lost'),
+    rucksack      = require('gulp-rucksack'),
+    jade          = require('gulp-jade'),
+    path          = require('path'),
+    fs            = require('fs'),
+    concat        = require('gulp-concat'),
+    notify        = require('gulp-notify'),
+    gutil         = require('gulp-util'),
+    uglify        = require('gulp-uglify'),
+    nano          = require('gulp-cssnano'),
+    connect       = require('gulp-connect'),
+    plumber       = require('gulp-plumber');
+    // deploy        = require('gulp-gh-pages');
+
+// ===================================================
+// Config
+// ===================================================
+
+var folder = {
+  css: 'css',
+  scss: 'css/src',
+  js: 'js',
+  jade: 'jade',
+  dist: 'dist',
+  font: 'fonts',
+  bootstrap_font: 'node_modules/bootstrap/dist/fonts/',
+  bootstrap_css: 'node_modules/bootstrap/dist/css/',
+  prism_css: 'node_modules/prismjs/themes/',
+  bootstrap_js: 'node_modules/bootstrap/dist/js/',
+  prism_js: 'node_modules/prismjs/',
+  jquery_js: 'node_modules/jquery/dist/',
+}
+
+var glob = {
+  css: folder.css + '/*.css',
+  scss: folder.css + '/src/**/*.scss',
+  js: folder.js + '/**/*.js',
+  jade: [folder.jade + '/**/*.jade', '!/**/_*.jade'],
+  images: 'images/**/*',
+  libs: 'libs/**/*'
+};
+
+var onError = function(err) {
+  notify.onError({
+    title:    "Gulp error in " + err.plugin,
+    message:  "<%= error.message %>",
+    sound: "Beep"
+  })(err);
+  this.emit('end');
+};
+
+// ===================================================
+// Styles
+// ===================================================
+
+gulp.task('css', function () {
+
+  var processors = [
+    autoprefixer({browsers: ['last 2 versions']}),
+    mqpacker({sort: true}),
+    lost()
+  ];
+
+  var stream = gulp.src(folder.scss + '/*.scss')
+    .pipe(plumber({
+      errorHandler: onError
+    }))
+    .pipe( sourcemaps.init() )
+    .pipe( sass() )
+    .pipe( nano( {
+      mergeRules: true
+    }) )
+    .pipe( postcss(processors) )
+    .pipe( rucksack() )
+    .pipe( sourcemaps.write('.') )
+    .pipe( gulp.dest(folder.css) )
+    .pipe( gulp.dest(folder.dist + '/css') )
+    .pipe( connect.reload() );
+  return stream;
+
+});
+
+// ===================================================
+// Scripts
+// ===================================================
+
+//copy vendor scripts from drupal to make them available for the styleguide
+gulp.task('script-drupal', function() {
+  return gulp.src([
+    folder.js_drupal + '/misc/drupal.js',
+    folder.js_drupal + '/misc/forms.js',
+  ])
+  .pipe( concat('drupal-core.js') )
+  .pipe( gulp.dest(folder.dist + '/js') );
+});
+
+// ===================================================
+// Import Prism CSS
+// ===================================================
+
+gulp.task('prism-css', function() {
+  stream = gulp.src(folder.prism_css + '/prism.css')
+    .pipe( gulp.dest(folder.dist + '/css/')  )
+  return stream;
+});
+
+// ===================================================
+// Import Prism JS
+// ===================================================
+
+gulp.task('prism-js', function() {
+  stream = gulp.src(folder.prism_js + '/prism.js')
+    .pipe( gulp.dest(folder.dist + '/js/')  )
+  return stream;
+});
+
+// ===================================================
+// Import jQuery JS
+// ===================================================
+
+gulp.task('jquery-js', function() {
+  stream = gulp.src(folder.jquery_js + '/jquery.min.js')
+    .pipe( gulp.dest(folder.js) )
+    .pipe( gulp.dest(folder.dist + '/js/')  )
+  return stream;
+});
+
+// ===================================================
+// Import Bootstrap assets
+// ===================================================
+
+gulp.task('bootstrap-css', function() {
+  stream = gulp.src(folder.bootstrap_css + '/bootstrap.min.css')
+    .pipe( gulp.dest(folder.css) )
+    .pipe( gulp.dest(folder.dist + '/css/')  )
+  return stream;
+});
+
+gulp.task('bootstrap-js', function() {
+  stream = gulp.src(folder.bootstrap_js + '/bootstrap.min.js')
+    .pipe( gulp.dest(folder.js) )
+    .pipe( gulp.dest(folder.dist + "/js") )
+  return stream;
+});
+
+gulp.task('bootstrap-font', function() {
+  stream = gulp.src(folder.bootstrap_font + '/*')
+    .pipe( gulp.dest(folder.font) )
+    .pipe( gulp.dest(folder.dist + "/fonts") )
+  return stream;
+});
+
+// ===================================================
+// Template file (Jade)
+// ===================================================
+
+gulp.task('jade', function() {
+
+  return gulp.src(glob.jade)
+    .pipe(plumber({
+      handleError: onError
+    }))
+    .pipe(jade({
+      pretty: true
+    })) // pipe to jade plugin
+    .pipe(gulp.dest(folder.dist)); // tell gulp our output folder
+});
+
+// ===================================================
+// Images
+// ===================================================
+
+gulp.task('images', function() {
+  stream = gulp.src(glob.images)
+    .pipe( gulp.dest(folder.dist + '/images') )
+    .pipe( connect.reload() );
+  return stream;
+});
+
+// ===================================================
+// Extras
+// ===================================================
+
+gulp.task('libs', function() {
+  stream = gulp.src(glob.libs)
+    .pipe( gulp.dest(folder.dist + '/libs') )
+  return stream;
+});
+
+// ===================================================
+// Set up a server
+// ===================================================
+
+gulp.task('connect', function() {
+  connect.server({
+    root: [folder.dist],
+    livereload: true,
+    port: 5000
+  });
+});
+
+
+// ===================================================
+// Watch dev tasks
+// ===================================================
+
+gulp.task('watch', function() {
+  gulp.watch([
+    glob.scss
+  ], ['css']);
+
+  gulp.watch([
+    folder.jade + '/**/*'
+  ], ['jade']);
+
+  gulp.watch([
+    glob.images
+  ], ['images']);
+
+});
+
+// ===================================================
+// Deploy to github pages branch
+// ===================================================
+
+gulp.task('deploy', ['build'], function() {
+  return gulp.src([folder.dist + '/**/*'])
+    .pipe( deploy() );
+});
+
+
+// ===================================================
+// Run this one time when you install the project so you have all files in the dist folder
+// ===================================================
+gulp.task('init', ['script-drupal', 'jquery-js', 'bootstrap-css', 'prism-js', 'prism-css', 'bootstrap-font', 'bootstrap-js']);
+gulp.task('build', ['css', 'jade', 'images', 'script-drupal', 'prism-js', 'prism-css', 'jquery-js', 'bootstrap-css', 'bootstrap-font', 'bootstrap-js']);
+gulp.task('default', ['css', 'jade', 'connect', 'watch']);
