42f509755d680de5d455b0d1541516f8e3883b90
Serhiy Chornobay <sergij.chornobay@lemberg.co.uk>
2016-06-24 12:28:10 +0300

    WIM-43 - Fixed styles for short teasers.

--- themes/wim/templates/node/node--short.tpl.php
+++ themes/wim/templates/node/node--short.tpl.php
@@ -80,34 +80,20 @@
  * @ingroup templates
  */
 ?>
-<article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-  <?php if ((!$page && !empty($title)) || !empty($title_prefix) || !empty($title_suffix) || $display_submitted): ?>
-    <header>
-      <?php print render($title_prefix); ?>
-      <?php if (!$page && !empty($title)): ?>
-        <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php if ($display_submitted): ?>
-        <span class="submitted">
-      <?php print $user_picture; ?>
-      <?php print $submitted; ?>
-    </span>
-      <?php endif; ?>
-    </header>
-  <?php endif; ?>
+<article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> node-teaser node-teaser-short clearfix"<?php print $attributes; ?>>
   <?php
   // Hide comments, tags, and links now so that we can render them later.
   hide($content['comments']);
   hide($content['links']);
   hide($content['field_tags']);
-  print render($content);
   ?>
-  <?php if (!empty($content['field_tags']) || !empty($content['links'])): ?>
-    <footer>
-      <?php print render($content['field_tags']); ?>
-      <?php print render($content['links']); ?>
-    </footer>
-  <?php endif; ?>
-  <?php print render($content['comments']); ?>
+  <div class="teaser-body">
+    <?php if (!$page && !empty($title)): ?>
+      <h3<?php print $title_attributes; ?>>
+        <a href="<?php print $node_url; ?>"><?php print $title; ?></a>
+      </h3>
+    <?php endif; ?>
+    <?php print render($content);
+    ?>
+  </div>
 </article>
