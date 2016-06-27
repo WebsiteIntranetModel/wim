5fb9ee2eae68d45624d6ac9f616242640ef7f21b
kedramon <volodymyr.kenidra@lemberg.co.uk>
2016-06-27 15:33:47 +0300

    Revert "Fixing codestyle for WIMMinkContext.php file."
    This reverts commit 26f19341922afc2c1f5527973a725f1665b7053e.

--- behat/features/bootstrap/WIMMinkContext.php
+++ behat/features/bootstrap/WIMMinkContext.php
@@ -1,63 +1,37 @@
 <?php
 
 /**
- * This file contains function for Mink
- *
- * PHP version 5.6
- *
- * @category MinkContext
- * @package  Behat
- * @author   Display Name <username@example.com>
- * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
- * @link     none
+ * @file
+ * This file contains function for Mink.
  */
 
 use Drupal\DrupalExtension\Context\MinkContext;
 
 /**
  * Defines application features from the specific context.
- *
- * @category WIMMinkContext
- * @package  Behat
- * @author   Display Name <username@example.com>
- * @author   Display Name <username@example.com>
- * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
- * @link     none
  */
-class WIMMinkContext extends MinkContext
-{
-    /**
-     * Makes the step case insensitive.
-     *
-     * @param string $heading heading
-     * @param string $region  region
-     *
-     * @override MinkContext::assertRegionHeading()
-     * @throws   \Exception
-     * @return   void
-     */
-    public function assertRegionHeading($heading, $region)
-    {
-        $regionObj = $this->getRegion($region);
-        $heading = strtolower($heading);
+class WIMMinkContext extends MinkContext {
 
-        foreach (array('h1', 'h2', 'h3', 'h4', 'h5', 'h6') as $tag) {
-            $elements = $regionObj->findAll('css', $tag);
-            if (!empty($elements)) {
-                foreach ($elements as $element) {
-                    if (trim(strtolower($element->getText())) === $heading) {
-                        return;
-                    }
-                }
-            }
+  /**
+   * Makes the step case insensitive.
+   *
+   * @override MinkContext::assertRegionHeading()
+   */
+  public function assertRegionHeading($heading, $region) {
+    $regionObj = $this->getRegion($region);
+
+    foreach (array('h1', 'h2', 'h3', 'h4', 'h5', 'h6') as $tag) {
+      $elements = $regionObj->findAll('css', $tag);
+      if (!empty($elements)) {
+        foreach ($elements as $element) {
+          if (trim(strtolower($element->getText())) === strtolower($heading)) {
+            return;
+          }
         }
-
-        $message = sprintf(
-            'The heading "%s" was not found in the "%s" region on the page %s',
-            $heading,
-            $region,
-            $this->getSession()->getCurrentUrl()
-        );
-        throw new \Exception($message);
+      }
     }
+
+    throw new \Exception(sprintf('The heading "%s" was not found in the "%s" region on the page %s', $heading, $region, $this->getSession()->getCurrentUrl()));
+  }
+
 }
