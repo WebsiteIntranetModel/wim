ad1fe6bbd573295c2768ced3a10154268b93d15c
ozin <mykhailo.gurei@lemberg.co.uk>
2016-08-26 11:55:11 +0300

    WIM-630 - Fixed template.

--- themes/wim/templates/system/page.tpl.php
+++ themes/wim/templates/system/page.tpl.php
@@ -154,7 +154,7 @@
       <section<?php print $content_column_class; ?>>
         <a id="main-content"></a>
         <?php print render($title_prefix); ?>
-        <?php if (!empty($title) && !$front_page): ?>
+        <?php if (!empty($title) && !$is_front): ?>
           <h1 class="page-header"><?php print $title; ?></h1>
         <?php endif; ?>
         <?php print render($title_suffix); ?>
