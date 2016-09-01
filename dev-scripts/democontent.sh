c8ca50159272ac4b09b9f55cd51ef05b99401f33
ozin <mykhailo.gurei@lemberg.co.uk>
2016-09-01 16:18:44 +0300

    Fixed folder for democontent script.

--- dev-scripts/democontent.sh
+++ dev-scripts/democontent.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Script fo adding demo content to the site.
+cd /var/www/html/;
+
 drush en wim_fixtures -y
 
 # Check if first argument given is "remove". In that case we remove demo content,
