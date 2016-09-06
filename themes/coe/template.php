f9ad3cd2ce4cb7186ea251bbe17e12286821ce78
Peter Polman <info@peterpolman.nl>
2016-09-06 15:14:09 +0200

    Added coevorden theme for testing felix blocks functionality.

---
+++ themes/coe/template.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @file
+ * The primary PHP file for the WIM STARTERKIT theme.
+ *
+ * This file should only contain light helper functions and point to stubs in
+ * other files containing more complex functions.
+ */
+
+$includes = drupal_get_path('theme', 'coe') . '/includes/';
+
+// Include preprocess.
+require_once $includes . 'preprocess.inc';
+// Include alter.
+require_once $includes . 'alter.inc';
+// Include theme.
+require_once $includes . 'theme.inc';
