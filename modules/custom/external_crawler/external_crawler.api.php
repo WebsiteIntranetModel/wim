3207fdc7736d686537addcaa6508263d362ac155
ozin <mykhailo.gurei@lemberg.co.uk>
2016-09-15 17:35:32 +0300

    WIM-946 by ozin: Fixed code style.

--- modules/custom/external_crawler/external_crawler.api.php
+++ modules/custom/external_crawler/external_crawler.api.php
@@ -1,11 +1,14 @@
 <?php
 
 /**
+ * @file
+ * API for External Crawler module.
+ */
+
+/**
  * Implements hook_external_crawler_document_alter().
- * 
+ *
  * Alter a document before indexing.
- * 
- * @param ApacheSolrDocument $document
  */
 function hook_external_crawler_document_alter(ApacheSolrDocument &$document) {
   $document->addField('foo', 'bar');
