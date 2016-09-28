5575bf36f39d87d066c7f4cbedffdcacb4cf8662
ozin <mykhailo.gurei@lemberg.co.uk>
2016-09-28 18:26:24 +0300

    WIM-729 - Added pause buton for slider.

--- modules/custom/slick_paragraphs/js/slick_paragraphs.js
+++ modules/custom/slick_paragraphs/js/slick_paragraphs.js
@@ -9,10 +9,33 @@
       var slickParagraphsSettings = settings.slickParagraphs;
 
       $.each(slickParagraphsSettings, function (sliderClass, sliderOptions) {
-        $('.' + sliderClass + ' .field-items').once('slick-paragraphs').slick(
+        var $slider = $('.' + sliderClass + '>.field-items');
+        $slider.once('slick-paragraphs').slick(
           sliderOptions
         );
+
+        // Add pause button to slider.
+        if (sliderOptions.pause) {
+          $slider.once('slick-pause').append(Drupal.theme('slickPause'));
+          $('.slick-pause', $slider).on('click', function () {
+            var $pauseBtn = $(this);
+            if ($pauseBtn.hasClass('paused')) {
+              $slider.slick('slickPlay');
+              $pauseBtn.removeClass('paused');
+            } else {
+              $slider.slick('slickPause');
+              $pauseBtn.addClass('paused');
+            }
+          })
+        }
       });
     }
+  };
+
+  /**
+   * docReaderLink theme function.
+   */
+  Drupal.theme.prototype.slickPause = function () {
+    return '<button type="button" data-role="none" class="slick-pause" aria-label="Pause" role="button">' + Drupal.t('Pause') + '</button>';
   };
 })(jQuery);
