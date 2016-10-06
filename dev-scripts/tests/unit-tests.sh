b984eebce84ee3c76e5be449bcb1d382d0cd68b2
Volodymyr Kenidra <volodymyr.kenidra@lemberg.co.uk>
2016-10-06 12:26:59 +0300

    WIM-1072 by kedramon: Addded functional testing and rebuid needed for XML sitemap.

--- dev-scripts/tests/unit-tests.sh
+++ dev-scripts/tests/unit-tests.sh
@@ -4,3 +4,4 @@ drush en simpletest -y;
 
 # Run simple tests for WIM.
 php /var/www/html/scripts/run-tests.sh WIM
+php /var/www/html/scripts/run-tests.sh --class "XMLSitemapUnitTest,XMLSitemapMenuFunctionalTest,XMLSitemapNodeFunctionalTest"
