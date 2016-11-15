f005e27d367f8b4a92ab956397d0f2ac42df6d5b
Peter Polman <info@peterpolman.nl>
2016-11-15 14:01:32 +0100

    WIM-1267 - Using the role attr here is redundant.

--- themes/wimbase/templates/system/page.tpl.php
+++ themes/wimbase/templates/system/page.tpl.php
@@ -107,7 +107,7 @@
 
       <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
         <div class="navbar-collapse collapse">
-          <nav role="navigation">
+          <nav>
             <?php if (!empty($primary_nav)): ?>
               <?php print render($primary_nav); ?>
             <?php endif; ?>
