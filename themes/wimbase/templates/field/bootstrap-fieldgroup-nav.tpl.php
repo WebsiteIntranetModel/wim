22669a495dfc19e2c9b9a21a50de676956c9c4e1
Vladyslav Nosikov <vladyslav.nosikov@lemberg.co.uk>
2016-12-07 12:32:04 +0200

    WIM-1275: Add keyboard controls and other accessibility adjustments for content tabs.

--- themes/wimbase/templates/field/bootstrap-fieldgroup-nav.tpl.php
+++ themes/wimbase/templates/field/bootstrap-fieldgroup-nav.tpl.php
@@ -8,8 +8,10 @@
  * - $wrapper_classes: The classes set on the wrapping div.
  * - $nav_classes: The classes set on the nav ul.
  * - $navs: An array of nav elements (tabs, pills)
+ * - $nav_role: Role for nav wrapper element.
  * - $pane_classes: The classes set on the panes containing content.
  * - $panes: An array of panes containing content.
+ * - $pane_role: Role for pane elements.
  * - $index: The index of the active tab/pane.
  *
  * @ingroup themeable
@@ -18,7 +20,7 @@
 
 <?php if (!$is_empty) : ?>
 
-  <div class="<?php print $wrapper_classes; ?>">
+    <div class="<?php print $wrapper_classes; ?>">
 
     <?php if ($flip) : ?>
       <div class="tab-content <?php print $pane_classes; ?>">
@@ -32,7 +34,7 @@
     <?php endif; ?>
 
     <?php if (!$is_single) : ?>
-      <ul class="nav <?php print $nav_classes; ?>">
+      <ul class="nav <?php print $nav_classes; ?>" role="<?php print $nav_role; ?>">
         <?php foreach ($navs as $index => $nav) : ?>
             <?php $active_class = $index === $active ? 'active' : '';?>
             <li class="<?php print $nav['classes']; ?> <?php print $active_class; ?>">
@@ -46,7 +48,7 @@
       <div class="tab-content <?php print $pane_classes; ?>">
         <?php foreach ($panes as $index => $pane) : ?>
             <?php $active_class = $index === $active ? 'active' : ''; ?>
-            <div id="<?php print $pane['id']; ?>" class="tab-pane <?php print $active_class; ?>">
+            <div id="<?php print $pane['id']; ?>" class="tab-pane <?php print $active_class; ?>" role="<?php print $pane_role; ?>">
             <?php print $pane['content']; ?>
           </div>
         <?php endforeach; ?>
