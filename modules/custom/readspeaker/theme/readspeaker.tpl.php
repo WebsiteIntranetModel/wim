1674942db4e566f7d92416484a5b09e036486f72
Marcel de Ruiter <m.deruiter@wowww.nl>
2020-06-12 09:10:58 +0200

    New readspeaker implementation (other cdn).

--- modules/custom/readspeaker/theme/readspeaker.tpl.php
+++ modules/custom/readspeaker/theme/readspeaker.tpl.php
@@ -1,21 +1,32 @@
 <?php
+
 /**
  * @file
- * Template for Read Speaker.
+ * Template for Readspeaker webReader.
  *
  * Available variables:
- * - $title: URL title.
- * - $customer_id: Customer id.
- * - $lang: Language code.
- * - $url: Current page URL.
- * - $button: Listen button text.
+ * @var string $customer_id
+ *   Readspeaker customer id.
+ * @var string $lang_code
+ *   Readspeaker language code (e.g. en_uk, nl_nl).
+ * @var string $title
+ *   Readspeaker popup title.
+ * @var url $url
+ *   Current page absolute url.
+ * @var string $button
+ *   Listen button text.
  */
 ?>
-<div id="readspeaker-button" class="rs_skip rsbtn rs_preserve">
-    <a class="rsbtn_play" title="<?php print $title; ?>"
-       href="//app.readspeaker.com/cgi-bin/rsent?customerid=<?php print $customer_id; ?>&amp;lang=<?php print $lang; ?>&amp;readid=page-content&amp;url=<?php print $url; ?>">
-        <span class="rsbtn_left rsimg rspart"><span class="rsbtn_text">
-                <span><?php print $button; ?></span></span></span>
-        <span class="rsbtn_right rsimg rsplay rspart"></span>
+
+<div>
+  <div id="readspeaker_button" class="rs_skip rsbtn rs_preserve">
+    <a title="<?php print $title; ?>" href="//app-eu.readspeaker.com/cgi-bin/rsent?customerid=<?php print $customer_id; ?>&lang=<?php print $lang_code; ?>&readid=content&url=" rel="nofollow" class="rsbtn_play" accesskey="L" aria-expanded="false" role="label">
+      <span class="rsbtn_left rsimg rspart">
+        <span class="rsbtn_text">
+          <span><?php print $button; ?></span>
+        </span>
+      </span>
+      <span class="rsbtn_right rsimg rsplay rspart"></span>
     </a>
+  </div>
 </div>
